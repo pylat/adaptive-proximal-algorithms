5b97c71c388d615cddfecd9debf795092df43b4d
Lorenzo Stella <stellalo@amazon.de>
2023-05-20 09:14:51 +0200

    rebase

--- experiments/least_absolute_deviation/runme.jl
+++ experiments/least_absolute_deviation/runme.jl
@@ -1,46 +1,50 @@
 include(joinpath(@__DIR__, "..", "autodiff.jl"))
 include(joinpath(@__DIR__, "..", "counting.jl"))
 include(joinpath(@__DIR__, "..", "recording.jl"))
-include(joinpath(@__DIR__, "..", "adaptive_proximal_algorithms.jl"))
 include(joinpath(@__DIR__, "..", "libsvm.jl"))
 
 
 using LinearAlgebra
 using SparseArrays
-using ProximalAlgorithms: VuCondat
-# using Statistics
-
 using Plots
 using LaTeXStrings
 using DelimitedFiles
-
-using Random 
-
+using Random
 using ProximalCore: Zero
 using ProximalOperators: NormL1, NormL2, Translate
+using AdaProx
 
 pgfplotsx()
 
-# square root lasso
+# least absolute deviation
 
-function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol = 1e-5, maxit = 1000, t= 1.0, seed = 0) where {T}
-    @info "Start square root lasso ($filename)"
+function run_least_absolute_deviation(
+    filename,
+    ::Type{T} = Float64;
+    lambda = 1e-1,
+    seed = 0,
+    tol = 1e-5,
+    maxit = 1000,
+    t = 1.0,
+) where {T}
+    @info "Start run_least_absolute_deviation ($filename)"
 
     Random.seed!(seed)
+
     X, y = load_libsvm_dataset(filename, T)
 
     m, n = size(X)
 
     f = Zero()
     g = NormL1(lambda)
-    h = Translate(NormL2(), -y)
+    h = Translate(NormL1(), -y)
     A = hcat(Matrix(X), ones(m, 1))
 
     Lf = lambda
 
     norm_A = norm(A)
 
-    solx, soly, numit, record_vc = vu_condat(
+    solx, soly, numit, record_vc = AdaProx.vu_condat(
         zeros(n + 1),
         zeros(m);
         f = f,
@@ -53,9 +57,9 @@ function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol
         tol = tol,
         record_fn = record_pd,
     )
-    @info "Vu-Condat" numit (f(solx) + h(A * solx))
+    @info "Vu-Condat" numit (f(solx) + h(A * solx)) count(!iszero, solx)
 
-    solx, soly, numit, record_mp = malitsky_pock(
+    solx, soly, numit, record_mp = AdaProx.malitsky_pock(
         zeros(n + 1),
         zeros(m);
         f = f,
@@ -68,7 +72,7 @@ function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol
         tol = tol,
         record_fn = record_pd,
     )
-    @info "Malitsky-Pock" numit (f(solx) + h(A * solx))
+    @info "Malitsky-Pock" numit (f(solx) + h(A * solx)) count(!iszero, solx)
 
     # solx, soly, numit, record_apd = adaptive_primal_dual(
     #     zeros(n + 1),
@@ -82,9 +86,9 @@ function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol
     #     tol = tol,
     #     record_fn = record_pd,
     # )
-    # @info "Adaptive PD" numit (f(solx) + h(A * solx))
+    # @info "Adaptive PD" numit (f(solx) + h(A * solx)) count(!iszero, solx)
 
-    solx, soly, numit, record_fapd = adaptive_linesearch_primal_dual(
+    solx, soly, numit, record_fapd = AdaProx.adaptive_linesearch_primal_dual(
         zeros(n + 1),
         zeros(m);
         f = f,
@@ -96,7 +100,7 @@ function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol
         tol = tol,
         record_fn = record_pd,
     )
-    @info "Fully Adaptive PD" numit (f(solx) + h(A * solx))
+    @info "Fully Adaptive PD" numit (f(solx) + h(A * solx)) count(!iszero, solx)
 
     @info "Collecting plot data"
 
@@ -111,18 +115,21 @@ function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol
     @info "exporting data"
 
     save_labels = Dict(
-        "Vu-Condat" => "Vu-Condat", 
+        "Vu-Condat" => "Vu-Condat",
         # "adaPD"     => "adaPD",
-        "MP"        => "MP",
+        "MP" => "MP",
         "adaPD+" => "AdaPD+",
-        )
-    p = Int(floor(t*1000)) # potential identifier for later!
-    lam = lambda 
+    )
+    p = Int(floor(t * 1000)) # potential identifier for later!
+    lam = lambda
 
     for k in keys(to_plot)
         d = length(to_plot[k][:A_evals])
         rr = Int(ceil(d / 100)) # keeping at most 50 data points
-        output = [(to_plot[k][:A_evals]+ to_plot[k][:At_evals]) max.(1e-14, to_plot[k][:norm_res])]
+        output = [(to_plot[k][:A_evals] + to_plot[k][:At_evals]) max.(
+            1e-14,
+            to_plot[k][:norm_res],
+        )]
         red_output = output[1:rr:end, :]
         filename = "$(save_labels[k])_$(m)_$(n)_$(Int(ceil(lam * 100)))_$(Int(p)).txt"
         filepath = joinpath(@__DIR__, "convergence_plot_data", filename)
@@ -135,7 +142,7 @@ function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol
     @info "Plotting"
 
     plot(
-        title = "square root lasso ($(basename(filename))) with lam$(Int(floor(lambda*100))), t$(Int(floor(t*100)))",
+        title = "run_least_absolute_deviation ($(basename(filename))) with lam$(Int(floor(lambda*100))), t$(Int(floor(t*100)))",
         xlabel = "#passes through data",
         ylabel = L"\|v\|",
     )
@@ -147,17 +154,38 @@ function run_square_root_lasso(filename, ::Type{T} = Float64; lambda = 1e-1, tol
             label = k,
         )
     end
-    savefig("convergence_SRL_$(basename(filename))_lam$(Int(floor(lambda*100)))_t$(Int(floor(t*100))).pdf")
+    savefig(
+        joinpath(
+            @__DIR__,
+            "convergence_LAD_$(basename(filename))_lam$(Int(floor(lambda*100)))_t$(Int(floor(t*100))).pdf",
+        )
+    )
 end
 
 
-function main(;maxit = 5000)
+
+function main(; maxit = 1000)
     # t is the ratio between the primal and the dual stepsizes 
-    for t in [1.0], lam in [1e-1, 1e1]
-        run_square_root_lasso(joinpath(@__DIR__, "../", "datasets", "cpusmall_scale"), maxit = maxit, tol = 1e-5, t= t)
-        run_square_root_lasso(joinpath(@__DIR__, "../", "datasets", "abalone"), maxit = maxit, tol = 1e-5, t = t)
-        run_square_root_lasso(joinpath(@__DIR__, "../", "datasets", "housing_scale"), maxit = maxit, tol = 1e-5, t = t)
-    end 
+    for t in [2.0], lam in [1e-1, 1e1]
+        run_least_absolute_deviation(
+            joinpath(@__DIR__, "../", "datasets", "cpusmall_scale"),
+            maxit = maxit,
+            tol = 1e-5,
+            t = t,
+        )
+        run_least_absolute_deviation(
+            joinpath(@__DIR__, "../", "datasets", "abalone"),
+            maxit = maxit,
+            tol = 1e-5,
+            t = t,
+        )
+        run_least_absolute_deviation(
+            joinpath(@__DIR__, "../", "datasets", "housing_scale"),
+            maxit = maxit,
+            tol = 1e-5,
+            t = t,
+        )
+    end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__

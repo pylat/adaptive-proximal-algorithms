7bd8259b9381d0f9c2a02e66e3f78ea44e1ed6fe
Lorenzo Stella <stellalo@amazon.de>
2023-09-10 21:09:20 +0200

    Update dual SVM script

--- experiments/logging.jl
+++ experiments/logging.jl
@@ -1,9 +1,22 @@
 using JSON
 using Logging
 using LoggingExtras
+using Dates
 using AdaProx
 
-function get_logger(path, keys=nothing)
+const date_format = "yyyy-mm-dd HH:MM:SS"
+
+timestamped(logger) = TransformerLogger(logger) do log
+    merge(log, (; message = "[$(Dates.format(now(), date_format))] $(log.message)"))
+end
+
+function is_logstep(base, it)
+    scale = floor(Int, log(base, it))
+    step = base ^ scale
+    return mod(it, step) == 0
+end
+
+function get_logger(path, keys=nothing, base=10)
     format_logger = if keys === nothing
         FormatLogger(path) do io, args
             println(io, JSON.json(args.kwargs))
@@ -14,13 +27,16 @@ function get_logger(path, keys=nothing)
         end
     end
     TeeLogger(
-        EarlyFilteredLogger(
-            args -> args.level != AdaProx.Record,
-            ConsoleLogger()
-        ),
+        current_logger(),
         EarlyFilteredLogger(
             args -> args.level == AdaProx.Record,
-            format_logger
+            TeeLogger(
+                format_logger,
+                ActiveFilteredLogger(
+                    args -> is_logstep(base, args.kwargs[:it]),
+                    timestamped(ConsoleLogger(AdaProx.Record))
+                )
+            )
         ),
     )
 end
